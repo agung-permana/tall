c16a810e98c77b8b0437a00c2a23ad426deedcbc
agung-permana <agungpermana490@gmail.com>
2025-03-03 16:28:09 +0700

    rename label for input

--- resources/views/livewire/account/edit.blade.php
+++ resources/views/livewire/account/edit.blade.php
@@ -8,7 +8,7 @@
                 
                 <form wire:submit.prevent="update" class="bg-white rounded px-8 pt-6 pb-8 mb-4">
                     <div class="md-5">
-                        <label for="" class="block font-medium mb-1">Nama</label>
+                        <label for="" class="block font-medium mb-1">Name</label>
                         <input type="text" wire:model="name" id="name" class=" appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3">
                     </div>
                     @error('name')
